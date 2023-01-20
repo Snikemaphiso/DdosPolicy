fec463b8090d698f197ae819ed9f448cf3882911
Robert Udah <airudah2000@gmail.com>
2023-01-20 01:22:34 +0000

    Adding scala meta

--- src/main/scala/controllers/MetaSandbox.scala
+++ src/main/scala/controllers/MetaSandbox.scala
@@ -1,5 +1,11 @@
 package controllers
 
-class MetaSandbox {
+import scala.meta._
 
+object MetaSandbox extends App {
+
+  val program = """object Main extends App { print("Hello!") }"""
+  val tree = program.parse[Source].get
+
+  println(tree.syntax)
 }
